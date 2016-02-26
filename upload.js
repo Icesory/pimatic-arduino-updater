271a2795417ac0ec56bffac2c5fae1180c58013e
Ronny <ronny_herzberg@web.de>
2016-02-27 00:23:04 +0100

    Add async test upload

--- upload.js
+++ upload.js
@@ -1,15 +1,37 @@
 var Avrgirl = require('avrgirl-arduino');
+var Promise = require('bluebird');
+
+var board = 'nano';
+var port = '/dev/ttyUSB0';
+var file = '/home/ronny/pimatic-dev/node_modules/pimatic-homeduino/arduino/Homeduino_nano328.hex';
+
 
 var avrgirl = new Avrgirl({
-  board: 'nano',
-  port: '/dev/ttyUSB0',
+  board: board,
+  port: port,
   debug: true
 });
-
-avrgirl.flash('/home/ronny/pimatic-dev/node_modules/pimatic-homeduino/arduino/Homeduino_nano328.hex', function (error) {
+console.info('Flash normal')
+avrgirl.flash(file, function (error) {
   if (error) {
     console.error(error);
   } else {
     console.info('done.');
   }
+
+  Promise.promisifyAll(Avrgirl.prototype)
+
+  var avrgirl = new Avrgirl({
+    board: board,
+    port: port,
+    debug: true
+  });
+  console.info('Flash async')
+  avrgirl.flashAsync(file).then( function (error) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.info('done.');
+    }
+  });
 });
